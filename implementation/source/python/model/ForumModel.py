440d82f3fac28c54188495e5e26535a30dd880fe
Carson Carroll <ccarroll511@yahoo.com>
2014-04-13 23:34:19 -0700

    Deleted a duplicate of ForumModel

--- implementation/source/python/model/ForumModel.py
+++
@@ -1,8 +0,0 @@
-class ForumModel:
-   #def __init__(self):
-
-   def SendMessage(self):
-      print('From Forum.SendMessage()')
-
-   def CloseForum(self):
-      print('From Forum.CloseMessage()')
