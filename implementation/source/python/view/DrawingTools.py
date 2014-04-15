153d9bd12d52acdde9ad9e827e878915eab2190e
Mike Sevilla <michaelsevilla@gmail.com>
2014-04-14 22:16:13 -0700

    Added drawing tools window

--- implementation/source/python/view/DrawingTools.py
+++ implementation/source/python/view/DrawingTools.py
@@ -6,6 +6,27 @@ class DrawingTools(wx.Frame):
    def __init__(self):
       super(DrawingTools, self).__init__(None, -1, 'Drawing Tools')
 
-      self.SetClientSizeWH(300, 400)
+      self.SetClientSizeWH(275, 65)
       self.SetBackgroundColour('#FFFFFFF')
+
+      ID_PENCIL_TOOL = wx.NewId()
+      ID_HAND_TOOL = wx.NewId()
+      ID_ATTACHMENT_TOOL = wx.NewId()
+      ID_TEXT_TOOL = wx.NewId()
+      ID_BASIC_SHAPES_TOOL = wx.NewId()
+
+      drawingTools = self.CreateToolBar()
+      pencilTool = drawingTools.AddLabelTool(ID_PENCIL_TOOL, 'Pencil', wx.Image('view\\PencilIcon.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap(),
+                                             shortHelp='Pencil Tool')
+      handTool = drawingTools.AddLabelTool(ID_HAND_TOOL, 'Hand', wx.Image('view\\HandIcon.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap(),
+                                           shortHelp='Hand Tool')
+      attachmentTool = drawingTools.AddLabelTool(ID_ATTACHMENT_TOOL, 'Attachment', wx.Image('view\\PaperClipIcon.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap(),
+                                                 shortHelp='Attachment Tool')
+      textTool = drawingTools.AddLabelTool(ID_TEXT_TOOL, 'Text', wx.Image('view\\TextIcon.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap(),
+                                           shortHelp='Text Tool')
+      basicShapesTool = drawingTools.AddLabelTool(ID_BASIC_SHAPES_TOOL, 'Basic Shapes', wx.Image('view\\BasicShapesIcon.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap(),
+                                                  shortHelp='Basic Shapes Tool')
+
+      drawingTools.Realize()
+
       self.Show()
