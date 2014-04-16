06c6d1f1c345d6ee0ca17a45fce09f47c674176a
Mike Sevilla <michaelsevilla@gmail.com>
2014-04-15 18:58:42 -0700

    Fixed forward slash on Drawing TOols

--- implementation/source/python/view/DrawingTools.py
+++ implementation/source/python/view/DrawingTools.py
@@ -16,15 +16,15 @@ class DrawingTools(wx.Frame):
       ID_BASIC_SHAPES_TOOL = wx.NewId()
 
       drawingTools = self.CreateToolBar()
-      pencilTool = drawingTools.AddLabelTool(ID_PENCIL_TOOL, 'Pencil', wx.Image('view\\PencilIcon.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap(),
+      pencilTool = drawingTools.AddLabelTool(ID_PENCIL_TOOL, 'Pencil', wx.Image('view//PencilIcon.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap(),
                                              shortHelp='Pencil Tool')
-      handTool = drawingTools.AddLabelTool(ID_HAND_TOOL, 'Hand', wx.Image('view\\HandIcon.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap(),
+      handTool = drawingTools.AddLabelTool(ID_HAND_TOOL, 'Hand', wx.Image('view//HandIcon.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap(),
                                            shortHelp='Hand Tool')
-      attachmentTool = drawingTools.AddLabelTool(ID_ATTACHMENT_TOOL, 'Attachment', wx.Image('view\\PaperClipIcon.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap(),
+      attachmentTool = drawingTools.AddLabelTool(ID_ATTACHMENT_TOOL, 'Attachment', wx.Image('view//PaperClipIcon.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap(),
                                                  shortHelp='Attachment Tool')
-      textTool = drawingTools.AddLabelTool(ID_TEXT_TOOL, 'Text', wx.Image('view\\TextIcon.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap(),
+      textTool = drawingTools.AddLabelTool(ID_TEXT_TOOL, 'Text', wx.Image('view//TextIcon.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap(),
                                            shortHelp='Text Tool')
-      basicShapesTool = drawingTools.AddLabelTool(ID_BASIC_SHAPES_TOOL, 'Basic Shapes', wx.Image('view\\BasicShapesIcon.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap(),
+      basicShapesTool = drawingTools.AddLabelTool(ID_BASIC_SHAPES_TOOL, 'Basic Shapes', wx.Image('view//BasicShapesIcon.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap(),
                                                   shortHelp='Basic Shapes Tool')
 
       drawingTools.Realize()
