b47e71e6d77004e902253f2e876fc2a7050fcff5
Carson Carroll <ccarroll511@yahoo.com>
2014-04-21 17:01:33 -0700

    Added Message model file

---
+++ implementation/source/python/model/Forum/Message.py
@@ -0,0 +1,8 @@
+class Message:
+   def __init__(self, name, time, text):
+      self.name = name
+      self.time = time
+      self.text = text
+
+   def ToString(self):
+      return self.name + " " + "[" + self.time + "]" + " " + self.text
