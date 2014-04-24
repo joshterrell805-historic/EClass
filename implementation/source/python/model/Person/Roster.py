2de131f6386719f6362021d1c5bff8394ec8c70e
Carson Carroll <ccarroll511@yahoo.com>
2014-04-24 00:38:28 -0700

    Updated m3-duties.html and added sorting to roster

--- implementation/source/python/model/Person/Roster.py
+++ implementation/source/python/model/Person/Roster.py
@@ -1,10 +1,15 @@
 class Roster:
    def __init__(self):
       self.studentsList = ["Carson Carroll", "Tim Anderson", "Ryan Ginsberg", "Mike Sevilla", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+      self.SortList(self.studentsList)
       self.remoteList = ["Josh Terrell", "Kevin Le", "Joel Wilcox"]
+      self.SortList(self.remoteList)
 
    def AddNewStudent(self):
       print('From Roster.AddNewStudent()')
 
    def RemoveStudent(self):
       print('From Roster.RemoveStudent()')
+
+   def SortList(self, list):
+      list.sort()
