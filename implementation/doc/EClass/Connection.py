062c2fae0dc5e423ba1eaf3456f6608ee9ef3572
Josh Terrell <joshterrell805@gmail.com>
2014-04-28 20:52:40 -0700

    Add more docs for EClass.connection

--- implementation/doc/EClass/Connection.py
+++ implementation/doc/EClass/Connection.py
@@ -16,6 +16,10 @@ class Connection:
    Note: listeners can be registered/unregistered whenever. The connection does
    not have to be made before registering a listener. It's advised to set up
    listeners before authenticating so that no messages are missed.
+
+   Note: "simple object" below refers to an object that can be encoded and
+   decoded by pickle. See pickle serialization library.
+
    @author: Josh Terrell jmterrel@calpoly.edu
    """
    def __init__(self):
@@ -27,6 +31,7 @@ class Connection:
    def getInstance():
       """
       @return: the single instance of Connection for this user.
+      @postcondition: isinstance(@return, Connection)
       """
       pass
    def authenticate(self, username, password, callback):
@@ -42,6 +47,8 @@ class Connection:
       succeeds or fails. This callback is passed an AuthenticationResponse
       (AuthenticationSuccess or AuthenticationFailure) as its only argument.
       @type callback: function passed one argument, AuthenticationResponse
+
+      @precondition: hasattr(callback, '__call__')
       """
       pass
    def hostPresentation(self, className, onStudentJoin, callback):
@@ -62,6 +69,9 @@ class Connection:
       @param callback: the callback to be called when the attempt to host
       has succeeded or failed.
       @type callback: function passed one argument, HostResponse
+
+      @precondition: hasattr(callback, '__call__') and
+      hasattr(onStudentJoin, '__call__')
       """
       pass
    def joinPresentation(self, className, callback):
@@ -76,6 +86,8 @@ class Connection:
       @param callback: a function to be called when the attempt to join succeeds
       or fails.
       @type callback: a function with one argument, a JoinResponse
+
+      @precondition: hasattr(callback, '__call__')
       """
       pass
    def registerStudentClassesListener(self, listener):
@@ -85,6 +97,8 @@ class Connection:
       from hosted to not hosted or visa versa.
       @param listener: the listener to be called whenever the classes change.
       @type listener: a function with one argument, an array of StudentClass
+
+      @precondition: hasattr(callback, '__call__')
       """
       pass
    def unregisterStudentClassesListener(self, listener):
@@ -93,6 +107,8 @@ class Connection:
       registerStudentClassesListener, or unregister all listeners. If listener 
       is None, this method unregisters all listeners. If listener is a funciton,
       this method unregisters the listener passed.
+      
+      @precondition: listener == None or hasattr(listener, '__call__')
       """
       pass
    def sendMessage(self, identifier, recipient, message):
@@ -116,8 +132,10 @@ class Connection:
       string, it is assumed to be the username of the student to send this
       message to.
       @type recipient: string or None
-      @param message: the message to send. Must be a simple python object.
-      @type message: object
+      @param message: the message to send. Must be a simple python object*.
+
+      @precondition: isinstance(identifier, string) && message
+      (recipient == None or isinstance(recipient, string))
       """
       pass
    def registerMessageListener(self, identifier, listener):
@@ -136,6 +154,8 @@ class Connection:
       sent this message to the presenter. The message is the object sent by
       the sender in sendMessage.
       @type listener: function with two arguments, sender and message
+
+      @precondition: hasattr(listener, '__call__')
       """
       pass
    def unregisterMessageListener(self, identifier, listener):
@@ -159,10 +179,60 @@ class Connection:
 
       If identifier is a string and listener is a funciton, unregister all
       message listeners associated with this identifier and function.
+
+      @precondition: (listener == None or hasattr(listener, '__call__')) and
+      (identifier == None or isinstance(identifier, string))
       """
       pass
 
-class AuthenticationResponse(object):
+class GenericResponse(object):
+   """
+   A generic response can either be a success or a failure. A failed generic
+   response has self.success == False, and succeeded has response.success ==
+   True. If the response is a success, this object is actually an instance of
+   GenericSuccess. If the response is a failure, the object is actually an
+   instance of GenericFailure. (both of which inherit from this class)
+
+   @ivar success: True for successful, false for fail
+   @type success: boolean (read-only)
+
+   Note: all subclasses must invoke the super constructor.
+   """
+   def __init__(self):
+      """
+      Don't forget to call the super constructor when inheriting from this
+      class!
+      """
+      pass
+
+class GenericSuccess(GenericResponse):
+   """
+   A generic success is a GenericResponse that succeeded. A generic success
+   may be subclassed to provide state to the recipient of this response.
+   """
+   def __init__(self):
+      """
+      Don't forget to call the super constructor when inheriting from this
+      class!
+      """
+      pass
+
+class GenericFailure(GenericResponse):
+   """
+   A generic failure is a GenericResponse that failed. A generic failure
+   may be subclassed to provide more state to the recipient of this response.
+   Generic failures have one state variable included, the reason for the
+   failure.
+   """
+   def __init__(self, reason):
+      """
+      Don't forget to call the super constructor when inheriting from this
+      class! (and don't forget to pass the reason (only arg)!)
+      """
+      super(GenericFailure, self).__init__()
+      self.reason = reason
+
+class AuthenticationResponse(GenericResponse):
    """
    This is the response received in the callback when authenticating. It is
    either an instance of AuthenticationSuccess or AuthenticationFailure.
@@ -170,22 +240,17 @@ class AuthenticationResponse(object):
    @type success: boolean (read-only)
    @author: Josh Terrell jmterrel@calpoly.edu
    """
-   @property
-   def success(self):
-      if self.__success == None:
-         self.__success = isinstance(self, AuthenticationSuccess)
-      return self.__success
+   pass
 
-class AuthenticationFailure(AuthenticationResponse):
+class AuthenticationFailure(GenericFailure):
    """
    @ivar reason: the reason authentication failed (eg 'invalid credentials')
    @type reason: string
    @author: Josh Terrell jmterrel@calpoly.edu
    """
-   def __init__(self, reason):
-      self.reason = reason
+   pass
 
-class AuthenticationSuccess(AuthenticationResponse):
+class AuthenticationSuccess(GenericSuccess):
    """
    @ivar role: the role (student/presenter) of this logged in user
    @type role: string 'student' or 'presenter'
@@ -252,7 +317,7 @@ class Student(object):
       self.firstName = firstName
       self.lastName = lastName
 
-class HostResponse(object):
+class HostResponse(GenericResponse):
    """
    Response passed to the callback when attempting to host. It is either an
    instance of HostSuccess or HostFailure
@@ -260,52 +325,47 @@ class HostResponse(object):
    @type success: boolean (read-only)
    @author: Josh Terrell jmterrel@calpoly.edu
    """
-   @property
-   def success(self):
-      if self.__success == None:
-         self.__success = isinstance(self, HostSuccess)
-      return self.__success
+   pass
 
-class HostSuccess(HostResponse): 
+class HostSuccess(GenericSuccess): 
    """
    @author: Josh Terrell jmterrel@calpoly.edu
    """
    pass
 
-class HostFailure(HostResponse):
+class HostFailure(GenericFailure):
    """
    @ivar reason: the reason hosting failed (eg 'not a presenter for this class')
    @type reason: string
    @author: Josh Terrell jmterrel@calpoly.edu
    """
-   def __init__(self, reason):
-      self.reason = reason
-
-class JoinResponse(object):
-   """
-   Response passed to the callback when attempting to join. It is either an
-   instance of JoinSuccess or JoinFailure
-   @ivar success: True for successful, false for fail
-   @type success: boolean (read-only)
-   @author: Josh Terrell jmterrel@calpoly.edu
-   """
-   @property
-   def success(self):
-      if self.__success == None:
-         self.__success = isinstance(self, JoinSuccess)
-      return self.__success
-
-class JoinSuccess(JoinResponse): 
-   """
-   @author: Josh Terrell jmterrel@calpoly.edu
-   """
    pass
 
-class JoinFailure(JoinResponse):
-   """
-   @ivar reason: the reason joining failed (eg 'not a student of this class')
-   @type reason: string
-   @author: Josh Terrell jmterrel@calpoly.edu
-   """
-   def __init__(self, reason):
-      self.reason = reason
+# class JoinResponse(object):
+#    """
+#    Response passed to the callback when attempting to join. It is either an
+#    instance of JoinSuccess or JoinFailure
+#    @ivar success: True for successful, false for fail
+#    @type success: boolean (read-only)
+#    @author: Josh Terrell jmterrel@calpoly.edu
+#    """
+#    @property
+#    def success(self):
+#       if self.__success == None:
+#          self.__success = isinstance(self, JoinSuccess)
+#       return self.__success
+# 
+# class JoinSuccess(JoinResponse): 
+#    """
+#    @author: Josh Terrell jmterrel@calpoly.edu
+#    """
+#    pass
+# 
+# class JoinFailure(JoinResponse):
+#    """
+#    @ivar reason: the reason joining failed (eg 'not a student of this class')
+#    @type reason: string
+#    @author: Josh Terrell jmterrel@calpoly.edu
+#    """
+#    def __init__(self, reason):
+#       self.reason = reason
