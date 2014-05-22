9ccc1f2e94f851408fa4a910422e096122ddd348
kle622 <kle622@gmail.com>
2014-05-21 23:07:40 -0700

    Added my test files to test.sh

--- testing/implementation/unit/test.sh
+++ testing/implementation/unit/test.sh
@@ -2,3 +2,5 @@ python Presentation/PresentationTest.py
 python Person/StudentTest.py
 python Connection/__init__.py
 python Presentation/DrawingToolsTest.py
+python Person/QuestionTest.py
+python Person/StudentPermissionTest.py
