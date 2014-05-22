2c61ee2c255b92f552860f7986b02197254c00f5
Carson Carroll <ccarroll511@yahoo.com>
2014-05-21 22:04:27 -0700

    Add student stuff

--- implementation/source/python/model/Person/Roster.py
+++ implementation/source/python/model/Person/Roster.py
@@ -1,7 +1,13 @@
+import sys
+sys.path.insert(0, '../../../../implementation/source/python/view')
+
+from Student import Student
+
 class Roster:
    def __init__(self):
       self.__view = None
       self.students = []
+      self.studentPanels = []
 
    def setView(self, view):
       self.__view = view;
@@ -49,17 +55,26 @@ class Roster:
    def setStudents(self, students):
       def toStudent(student):
          # temporary because there's errors when including student.
-         class Student():
-            pass
          newStudent = Student()
          newStudent.username = student['username']
          newStudent.present = False
-         # temporary... these should be passed to the constructor
          newStudent.firstname = student['firstname']
          newStudent.lastname = student['lastname']
+         # class Student():
+         #    pass
+         # newStudent = Student()
+         # newStudent.username = student['username']
+         # newStudent.present = False
+         # # temporary... these should be passed to the constructor
+         # newStudent.firstname = student['firstname']
+         # newStudent.lastname = student['lastname']
          return newStudent
       # contains Student objects
       self.students = map(toStudent, students)
       self.SortList()
+      rosterItem = None
+      for i in range(0, len(self.students)):
+         rosterItem = RosterItemPanel()
+         self.studentPanels.append(rosterItem)
       self.__view and self.__view.redraw()
    
--- testing/implementation/unit/Person/RosterTest.py
+++ testing/implementation/unit/Person/RosterTest.py
@@ -3,6 +3,7 @@ import sys
 sys.path.insert(0, '../../../../implementation/source/python/model/Person')
 
 from Roster import Roster
+from RosterItemPanel import RosterItemPanel
 
 class RosterTest(unittest.TestCase):
 
@@ -35,6 +36,16 @@ class RosterTest(unittest.TestCase):
       self.assertEqual(self.roster.students[1].firstname, 'Joel')
       self.assertEqual(self.roster.students[2].firstname, 'Josh')
       self.assertEqual(self.roster.students[3].firstname, 'Mike')
+
+   def test_studentPanels(self):
+      studs = [{'username' : 'jmterrel', 'firstname' : 'Josh', 'lastname' : 'Terrell'}, 
+      {'username' : 'ccarro03', 'firstname' : 'Carson', 'lastname' : 'Carroll'},
+      {'username' : 'jnwilcox', 'firstname' : 'Joel', 'lastname' : 'Wilcox'},
+      {'username' : 'mjsevill', 'firstname' : 'Mike', 'lastname' : 'Sevilla'}]
+
+      self.roster.setStudents(studs)
+
+      self.assertEqual(len(self.roster.studentPanels), 4)
 
 
 if __name__ == '__main__':
