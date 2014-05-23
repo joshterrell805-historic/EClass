0fab596675a0597d9d3b71c4fd8a0a4e678cebd5
Josh Terrell <joshterrell805@gmail.com>
2014-05-22 19:03:26 -0700

    Move connection test to end of test script
    It takes at least a second, usually 4 just in sleeping.

--- testing/implementation/unit/test.sh
+++ testing/implementation/unit/test.sh
@@ -1,8 +1,8 @@
 python Presentation/PresentationTest.py
 python Person/StudentTest.py
-python Connection/__init__.py
 python Presentation/DrawingToolsTest.py
 python Person/QuestionTest.py
 python Person/StudentPermissionsTest.py
 python Presentation/LayerTest.py
 python Presentation/LayerManagerModelTest.py
+python Connection/__init__.py
